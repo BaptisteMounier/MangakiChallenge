5f8ae7d5969b3499e6e75290145eb879079f4795
BaptisteMounier <baptiste.mounier@hotmail.fr>
2018-01-06 21:16:20 +0100

    Ajout du système de validation (mae)

--- src/v2/Main.java
+++ src/v2/Main.java
@@ -10,22 +10,33 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
 public class Main {
 
 	private static String pathGlobal = "datas/"+File.separator;
-	private static String pathOeuvres = "titles.csv";
+//	private static String pathOeuvres = "titles.csv";
 	private static String pathVotes = "watched.csv";
 	private static String pathSouhaits = "train.csv";
 	private static String pathEntrees = "test.csv";
 	private static String pathResultat = "resultat.csv";
+	private static String pathObjectif = "submission.csv";
 
 	private static int[][] votes;
 	private static int[][] souhaits;
 	private static int nombreUtilisateur;
 	private static int nombreOeuvre;
 
-	public static void main(String[] args) {
+	private static HashMap<Integer, Double> moyVotes;
+	private static HashMap<Integer, Double> moySouhaits;
 
+	private static ArrayList<String> objectif;
+	private static ArrayList<String> resultat;	
+
+	public static void main(String[] args) {
+		
+		long start = System.currentTimeMillis();
+		
 		nombreUtilisateur = 1982;
 		nombreOeuvre = 9896;
 		votes = new int[nombreUtilisateur+1][nombreOeuvre+1];
@@ -40,20 +51,16 @@ public class Main {
 				souhaits[i][j] = -1;
 			}
 		}
+		resultat = new ArrayList<String>();
+		objectif = new ArrayList<String>();
 
 		RecupDonnees();
+		CalculMoyennes();
 		Traitement();
-		//Validation();
-
-		double a = 1;
-		double b = 2;
-		double c = a/b;
-		System.out.println(c);
+		Validation();
 		
-		double d = 0;
-		double e = 0;
-		double f = d/e;
-		System.out.println(f);
+		System.out.println(System.currentTimeMillis()-start);
+		
 
 		//		for(Oeuvre oeuvre : listeOeuvres){
 		//			System.out.println(oeuvre);
@@ -69,7 +76,7 @@ public class Main {
 		//RecupDonnees_listeOeuvres(); // Inutile de connaître dans le cadre du TP
 		RecupDonnees_votes();
 		RecupDonnees_souhaits();
-		//RecupDonnees_listeCible();
+		RecupDonnees_objectif();
 	}
 
 	private static void Traitement(){
@@ -96,87 +103,74 @@ public class Main {
 					int id_utilisateur = Integer.parseInt(ligne[0]);
 					int id_oeuvre = Integer.parseInt(ligne[1]);
 
-					double totalVxW = 0;
-					double totalW = 0;
-					boolean voteUtilisable = false;
-					
+					double estimation_haut = 0;
+					double estimation_bas = 0;
+
 					for(int utilisateur = 0;utilisateur < nombreUtilisateur;utilisateur++){
+
+						double w = 0;
+						boolean utilisable = false;
 
 						if(votes[utilisateur][id_oeuvre] != -1){
 
-							double uv = 0;
-							double uvNorme = 0;
-							double uvNormeIdUtilisateur = 0;
-							double uvNormeUtilisateur = 0;
-							boolean utilisable = false;
+							double w_haut = 0;
+							double w_bas_gauche = 0;
+							double w_bas_droit = 0;
 
 							for(int oeuvre = 0;oeuvre < nombreOeuvre;oeuvre++){
-								if((votes[id_utilisateur][oeuvre] != -1) && (votes[utilisateur][oeuvre] != -1)){
-//									System.out.println(votes[id_utilisateur][oeuvre]);
-//									System.out.println(votes[utilisateur][oeuvre]);
-									uv += votes[id_utilisateur][oeuvre]*votes[utilisateur][oeuvre];
-									uvNormeIdUtilisateur += Math.pow(votes[id_utilisateur][oeuvre], 2);
-									uvNormeUtilisateur += Math.pow(votes[utilisateur][oeuvre], 2);
-									voteUtilisable = true;
+								if((votes[id_utilisateur][oeuvre] > 0) && (votes[utilisateur][oeuvre] > 0)){
+
+									w_haut += (votes[id_utilisateur][oeuvre] * votes[utilisateur][oeuvre]);
+									w_bas_gauche += Math.pow(votes[id_utilisateur][oeuvre], 2);
+									w_bas_droit += Math.pow(votes[utilisateur][oeuvre], 2);
 									utilisable = true;
+
 								}
 							}
 
 							if(utilisable){
-								uvNormeIdUtilisateur = Math.sqrt(uvNormeIdUtilisateur);
-								uvNormeUtilisateur = Math.sqrt(uvNormeUtilisateur);
-								uvNorme = uvNormeIdUtilisateur * uvNormeUtilisateur;
-								double w = uv / uvNorme;
-								double vw = votes[utilisateur][id_oeuvre]*w;
-								totalVxW += vw;
-								totalW += w;
+								w = w_haut/(Math.sqrt(w_bas_gauche*w_bas_droit));
+								estimation_haut += votes[utilisateur][id_oeuvre]*w;
+								estimation_bas += w;
+							}
+
+						}if(souhaits[utilisateur][id_oeuvre] != -1){
+
+							double w_haut = 0;
+							double w_bas_gauche = 0;
+							double w_bas_droit = 0;
+
+							for(int oeuvre = 0;oeuvre < nombreOeuvre;oeuvre++){
+								if((souhaits[id_utilisateur][oeuvre] > 0) && (souhaits[utilisateur][oeuvre] > 0)){
+
+									w_haut += (souhaits[id_utilisateur][oeuvre] * souhaits[utilisateur][oeuvre]);
+									w_bas_gauche += Math.pow(souhaits[id_utilisateur][oeuvre], 2);
+									w_bas_droit += Math.pow(souhaits[utilisateur][oeuvre], 2);
+									utilisable = true;
+
+								}
+							}
+
+							if(utilisable){
+								w = w_haut/(Math.sqrt(w_bas_gauche*w_bas_droit));
+								estimation_haut += souhaits[utilisateur][id_oeuvre]*w;
+								estimation_bas += w;
 							}
 
 						}
-					}
-					if(!voteUtilisable){
-						for(int utilisateur = 0; utilisateur < nombreUtilisateur;utilisateur++){
 
-							if(souhaits[utilisateur][id_oeuvre] > 0){
-
-								double uv = 0;
-								double uvNorme = 0;
-								double uvNormeIdUtilisateur = 0;
-								double uvNormeUtilisateur = 0;
-								boolean utilisable = false;
-
-								for(int oeuvre = 0;oeuvre < nombreOeuvre;oeuvre++){
-									if(souhaits[id_utilisateur][oeuvre] > 0 && souhaits[utilisateur][oeuvre] > 0){
-										uv += souhaits[id_utilisateur][oeuvre]*souhaits[utilisateur][oeuvre];
-										uvNormeIdUtilisateur += Math.pow(souhaits[id_utilisateur][oeuvre], 2);
-										uvNormeUtilisateur += Math.pow(souhaits[utilisateur][oeuvre], 2);
-										utilisable = true;
-									}
-								}
-
-								if(utilisable){
-									uvNormeIdUtilisateur = Math.sqrt(uvNormeIdUtilisateur);
-									uvNormeUtilisateur = Math.sqrt(uvNormeUtilisateur);
-									uvNorme = uvNormeIdUtilisateur * uvNormeUtilisateur;
-									double w = uv / uvNorme;
-									double vw = souhaits[utilisateur][id_oeuvre]*w;
-									totalVxW += vw;
-									totalW += w;
-								}
-
-							}
-
-						}
 					}
 
-					double estimation = totalVxW / totalW;
+					double estimation = estimation_haut / estimation_bas;
 
 					double percent = (double) nbligne/100016 *100;
-					System.out.print(percent+"\t");
-					System.out.println(id_utilisateur+"\t"+id_oeuvre+"\t"+estimation);
+					System.out.print("Avancement: "+percent+"\t");
+					System.out.println("Utilisateur: "+id_utilisateur+"\tOeuvre: "+id_oeuvre+"\tEstimation: "+estimation);
 
+					resultat.add(id_utilisateur+","+id_oeuvre+","+estimation);
 					bufferedWriter.write(id_utilisateur+","+id_oeuvre+","+estimation);
 					bufferedWriter.newLine();
+
 				}
 
 			}
@@ -190,43 +184,84 @@ public class Main {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		//
-		//		try {
-		//
-		//			// Assume default encoding.
-		//			FileWriter fileWriter;
-		//			fileWriter = new FileWriter(pathGlobal + pathResultat);
-		//			// Always wrap FileWriter in BufferedWriter.
-		//			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-		//
-		//			bufferedWriter.write("user_id,work_id,prob_willsee");
-		//			bufferedWriter.newLine();
-		//			for(Utilisateur utilisateur : listeCibles){
-		//				HashMap<Integer, Integer> souhaits = utilisateur.getSouhaits();
-		//				for(int oeuvre : souhaits.keySet()){
-		//					bufferedWriter.write(utilisateur.getId()+","+oeuvre+","+souhaits.get(oeuvre));
-		//					bufferedWriter.newLine();
-		//				}
-		//			}
-		//
-		//			bufferedWriter.close();
-		//
-		//		} catch (IOException e) {
-		//			// TODO Auto-generated catch block
-		//			e.printStackTrace();
-		//		}
 
 	}
-	//
-	//	private static void Validation(){
-	//		// Trouver et mettre un moyen de calcul de performance auc
-	//		// http://mark.goadrich.com/programs/AUC/
-	//		// http://www.edwardraff.com/jsat_docs/JSAT-0.0.7-javadoc/jsat/classifiers/evaluation/AUC.html#AUC--
-	//	}
-	//
-	//	private static void RecupDonnees_listeOeuvres(){
-	//		// Récupération de la liste des oeuvres
-	//	}
+
+	public static void CalculMoyennes(){
+		moyVotes = new HashMap<Integer, Double>();
+		for(int utilisateur = 0;utilisateur < nombreUtilisateur;utilisateur++){
+			double somme = 0;
+			double nombre = 0;
+			boolean utilisable = false;
+			for(int oeuvre = 0;oeuvre < nombreOeuvre;oeuvre++){
+				int vote = votes[utilisateur][oeuvre];
+				if(vote != -1){
+					somme += vote;
+					nombre++;
+					utilisable = true;
+				}
+			}
+			if(utilisable){
+				double moyenne = somme/nombre;
+				moyVotes.put(utilisateur, moyenne);
+			}
+		}
+
+		moySouhaits = new HashMap<Integer, Double>();
+		for(int utilisateur = 0;utilisateur < nombreUtilisateur;utilisateur++){
+			double somme = 0;
+			double nombre = 0;
+			boolean utilisable = false;
+			for(int oeuvre = 0;oeuvre < nombreOeuvre;oeuvre++){
+				int souhait = souhaits[utilisateur][oeuvre];
+				if(souhait != -1){
+					somme += souhait;
+					nombre++;
+					utilisable = true;
+				}
+			}
+			if(utilisable){
+				double moyenne = somme/nombre;
+				moySouhaits.put(utilisateur, moyenne);
+			}
+		}
+	}
+
+	private static void Validation(){
+		// Trouver et mettre un moyen de calcul de performance auc
+		// http://mark.goadrich.com/programs/AUC/
+		// http://www.edwardraff.com/jsat_docs/JSAT-0.0.7-javadoc/jsat/classifiers/evaluation/AUC.html#AUC--
+		
+	
+		if(objectif.size() == resultat.size()){
+			double erreurTotal = 0;
+			int nombreCas = 0;
+			int nombreCasNonResolu = 0;
+			for(int line = 0;line < objectif.size();line++){
+				String[] entreObj = objectif.get(line).split(",");
+				String[] entreRes = resultat.get(line).split(",");
+				if(entreObj[0].equals(entreRes[0]) && entreObj[1].equals(entreRes[1])){
+					double prediction = Double.parseDouble(entreRes[2]);
+					double reel = Double.parseDouble(entreObj[2]);
+					if(Double.isNaN(prediction)){
+						nombreCasNonResolu++;
+					}else{
+						double erreur = Math.sqrt(Math.pow(prediction - reel, 2));
+						erreurTotal += erreur;
+						nombreCas++;
+					}
+				}else{
+					System.out.println("L'objectif et le résultat ne comportent pas le même couple utilisateur/oeuvre.");
+				}
+			}
+			double mae = erreurTotal / nombreCas;
+			System.out.println("La valeur mae du système de recommandations est de "+mae+" et dispose de "+nombreCasNonResolu+" cas non résolu (NaN).");
+			System.out.println(erreurTotal+" "+nombreCas);
+		}else{
+			System.out.println("L'objectif et le résultat ne comportent pas le même nombre d'élément.");
+		}
+	
+	}
 
 	private static void RecupDonnees_votes(){
 		// Récupération des votes des utilisateurs
@@ -311,37 +346,28 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-	//
-	//	private static void RecupDonnees_listeCible(){
-	//	}
-	//
-	//	private static void ecritureResultats(){
-	//
-	//		try {
-	//
-	//			// Assume default encoding.
-	//			FileWriter fileWriter;
-	//			fileWriter = new FileWriter(pathGlobal + pathResultat);
-	//			// Always wrap FileWriter in BufferedWriter.
-	//			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-	//
-	//			bufferedWriter.write("user_id,work_id,prob_willsee");
-	//			bufferedWriter.newLine();
-	//			for(Utilisateur utilisateur : listeCibles){
-	//				HashMap<Integer, Integer> souhaits = utilisateur.getSouhaits();
-	//				for(int oeuvre : souhaits.keySet()){
-	//					bufferedWriter.write(utilisateur.getId()+","+oeuvre+","+souhaits.get(oeuvre));
-	//					bufferedWriter.newLine();
-	//				}
-	//			}
-	//
-	//			bufferedWriter.close();
-	//
-	//		} catch (IOException e) {
-	//			// TODO Auto-generated catch block
-	//			e.printStackTrace();
-	//		}
-	//
-	//	}
+
+	private static void RecupDonnees_objectif(){
+		// Récupération des votes des utilisateurs
+		try {
+			FileReader fileReader;
+			fileReader = new FileReader(pathGlobal + pathObjectif);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+			String line;
+
+			while((line = bufferedReader.readLine()) != null) {
+				if(!line.startsWith("user_id")){
+					objectif.add(line);
+				}
+			}
+			bufferedReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR : Fichier non trouvé !");
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
